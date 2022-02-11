601837f91db0ca6d002ae138592503790d0fd16d
vstorchak <vadim.storchak@utair.ru>
2022-02-11 19:50:44 +0300

    init commit

---
+++ src/test/java/com/example/journal_coast/JournalCoastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.journal_coast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalCoastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
